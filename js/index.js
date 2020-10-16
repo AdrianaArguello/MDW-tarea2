fb84379b08436b1af912bbcb9949eea7b21ba33d
AdrianaArguello <adriana.arguello2906@gmail.com>
2020-10-15 21:10:00 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
-
 var navbar = document.getElementById("home");
-
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
+    }
+}
+
+function navResponsive() {
+    var x = document.getElementById("navegacion");
+    if (x.className === "navegacion-right") {
+        x.className += " responsive";
+    } else {
+        x.className = "navegacion-right";
     }
 }
